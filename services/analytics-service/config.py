76ae3b84a3d6faac4f76fe6ed5e8cbf4480a9c30
Ruhao Yang <ruhao003@gmail.com>
2025-12-06 13:38:29 +0000

    Updated services to use real-time GTFS bus data and implement API KEY

--- services/analytics-service/config.py
+++ services/analytics-service/config.py
@@ -6,7 +6,7 @@ KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "bus_updates")
 
 # Postgres / PostGIS (BusDataDB)
 DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = int(os.getenv("DB_PORT", "5432"))
+DB_PORT = int(os.getenv("DB_PORT", 5432))
 DB_NAME = os.getenv("DB_NAME", "BusDataDB")
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "") # Password in .env
