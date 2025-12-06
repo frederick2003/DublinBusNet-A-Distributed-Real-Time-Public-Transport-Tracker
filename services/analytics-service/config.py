6e5831b3cabc5cdad823abb35142bd2c8a8b6855
Ruhao Yang <ruhao003@gmail.com>
2025-12-06 13:46:05 +0000

    Updated services to use real-time GTFS bus data and implement API KEY

--- services/analytics-service/config.py
+++ services/analytics-service/config.py
@@ -9,4 +9,4 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = int(os.getenv("DB_PORT", 5432))
 DB_NAME = os.getenv("DB_NAME", "BusDataDB")
 DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD") # Password in .env
+DB_PASS = os.getenv("DB_PASS", "")
